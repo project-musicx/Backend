3e36ca5bf1e7ad8e67272e72a598ffcc9b9b1b39
Kgdev <48225800+kennethoo@users.noreply.github.com>
2022-07-08 01:29:44 -0700

    new commit

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express= require("express")
 const app = express()
-app.get("/",(req,res)=>{
-    res.send("spotify player music")
+require("dotenv").config();
+const PORT= process.env.PORT||3000
+const querystring= require("querystring")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/login",(req,res)=>{
+    var scope = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id:process.env.CLIENT_ID,
+      scope: scope,
+      redirect_uri: process.env.REDRIECTURI,
+      state: "gjkktlnmjklmn"
+    }));
 })
-app.listen(3000)
+app.get("/logged",(req,res)=>{
+console.log(req.body)
+res.send("succed")
+})
+app.listen(PORT)
