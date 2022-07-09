965b408dcac90b19937e33bad7aa8188e35e4f35
Kgdev <48225800+kennethoo@users.noreply.github.com>
2022-07-09 00:43:27 -0700

    new codebase

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const app = express()
 const spotifyWebApi= require("spotify-web-api-node")
 require("dotenv").config();
 const PORT= process.env.PORT||3000
-const request= require("request")
-const querystring= require('node:querystring')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const scope= require("./utility/scope")
-
+const aboutMe= require("./shopifyApi/aboutMe")
+const playlistApi=require("./shopifyApi/playlist")
 const spotifyApi= new spotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret:process.env.CLIENT_SECRET,
     redirectUri: process.env.REDRIECTURI
 })
 
+app.get("/",(req,res)=>{
+  res.json("Welcome to out music sharing app")
+})
 app.get("/login",(req,res)=>{
     res.redirect(spotifyApi.createAuthorizeURL(scope))
 })
@@ -25,7 +27,7 @@ app.get("/logged",(req,res)=>{
     const state = req.query.state;
   
     if (error) {
-      console.error('Callback Error:', error);
+ 
       res.send(`Callback Error: ${error}`);
       return;
     }
@@ -39,31 +41,39 @@ app.get("/logged",(req,res)=>{
 
       spotifyApi.setAccessToken(access_token);
       spotifyApi.setRefreshToken(refresh_token);
-
-      console.log('access_token:', access_token);
-      console.log('refresh_token:', refresh_token);
-
-      console.log(
-        `Sucessfully retreived access token. Expires in ${expires_in} s.`
-      );
       res.send('Success! You can now close the window.');
 
       setInterval(async () => {
         const data = await spotifyApi.refreshAccessToken();
         const access_token = data.body['access_token'];
 
-        console.log('The access token has been refreshed!');
-        console.log('access_token:', access_token);
+
         spotifyApi.setAccessToken(access_token);
       }, expires_in / 2 * 1000);
     })
     .catch(error => {
-      console.error('Error getting Tokens:', error);
+    
       res.send(`Error getting Tokens: ${error}`);
     });
 
 
 });
+app.get("/my-spotify-date",(req,res)=>{
+const  aboutTheUser=  aboutMe(process.env.TOKEN)
+aboutTheUser.then((result)=>{
+  res.send(`Hello ${result.display_name} How are you ?`);
+}).catch((error)=>{
+res.send("Something went wrong")
+})
+})
 
+app.get("/my-playlist",(req,res)=>{
+  const myPlaylist=playlistApi(process.env.TOKEN, process.env.ID)
+  myPlaylist.then((result)=>{
+    res.send(JSON.stringify(result, undefined, 40))
+  }).catch((error)=>{
+  res.send("Something went wrong")
+  })
+})
 
 app.listen(PORT)
