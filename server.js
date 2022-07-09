341aac6b9b279f543469d112631b6653e99c508c
Kgdev <48225800+kennethoo@users.noreply.github.com>
2022-07-08 18:58:02 -0700

    get acces-token

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+
 const express= require("express")
 const app = express()
 require("dotenv").config();
 const PORT= process.env.PORT||3000
-const querystring= require("querystring")
+const request= require("request")
+const querystring= require('node:querystring')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
@@ -18,7 +20,37 @@ app.get("/login",(req,res)=>{
     }));
 })
 app.get("/logged",(req,res)=>{
-console.log(req.body)
-res.send("succed")
-})
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    if (state === null) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+       let authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+              code: code,
+              redirect_uri: process.env.REDRIECTURI,
+              grant_type: 'authorization_code'
+            },
+            headers: {
+              'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
+            },
+            json: true
+          };
+          request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body)
+              var access_token = body.access_token;
+              res.send({
+                'access_token': access_token
+              });
+            }
+          });
+            }
+});
+
+
 app.listen(PORT)
