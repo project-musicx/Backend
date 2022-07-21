03953e22ad8668c96750ccda0f13dc473efc837d
Ato Aidoo <77356776+atoaidoocr7@users.noreply.github.com>
2022-07-20 23:37:57 -0700

    New Changes

---
+++ configs/passport-setup.js
@@ -0,0 +1,49 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+const keys = require('../configs/keys')
+const User = require('../models/User')
+
+
+//Tell passport we want to use the Google Strategy
+//Takes in two params
+//1 - Strategu
+//2 - Call back ~ fires at some point during auth process
+
+passport.use(
+    new GoogleStrategy({
+        callbackURL: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    },
+    (accessToken, refreshToken, profile, done)=>{
+        
+        //passport call back function
+        //callback takes in a few parameters
+
+        //accessToken - 'used to fetch user data'
+        //refreshToken - 'refreshes the access token'
+        //profile - the response we want 
+        //done - we call this when done with call back funciton
+
+        console.log('call back function fired')
+        console.log(`access token is ${accessToken}`)
+        console.log(profile._json )
+                                
+        const gid = profile._json.sub
+        User.findOne( {googleId:gid} )
+        .then((currUser)=>{
+            if( currUser ){
+                console.log("user already exists " + currUser)
+            }
+            else{
+                new User({username:profile._json.name, googleId: gid}).save()
+                .then((newUser)=>{
+                    console.log('new user created ' + newUser)
+                })
+                .catch((err)=>{
+                    console.log('could not create user ' + err)
+                })
+            }
+        })
+    })
+)
