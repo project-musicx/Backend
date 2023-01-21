0cd41ef5ff408e786e2c1280db5422fbd5e953be
Kgdev <48225800+kennethoo@users.noreply.github.com>
2023-01-20 18:25:29 -0800

    new commit

--- redis/redisClient.js
+++ redis/redisClient.js
@@ -1,4 +1,5 @@
 const redis = require("redis");
-const redisClient = redis.createClient(6379);
+const { endpointUri, password } = require("./config").redis;
+const redisClient = redis.createClient(`redis://${endpointUri}`, { password });
 
 module.exports = redisClient;
